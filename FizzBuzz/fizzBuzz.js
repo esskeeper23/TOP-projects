0815429cd7d842a6a2efed86f5ed0204d7f0cb30
Daniela Martinez <rene2.martinez@outlook.com>
2022-05-02 21:06:11 -0700

    finish fizzBuzz project

---
+++ FizzBuzz/fizzBuzz.js
@@ -0,0 +1,22 @@
+
+function fizzBuzz(){
+
+    let answer = +prompt("Please enter the number you would like to FizzBuzz up to: ");
+
+        for(let i = 1; i <= answer; i++){
+
+        if (i % 3 === 0 && i % 15 === 0){
+            console.log("FizzBuzz")
+        }
+        else if (i % 5 === 0){
+            console.log("Buzz")
+        }
+        else if (i % 3 === 0){
+            console.log("Fizz")
+        }else{
+            console.log(i)
+        }
+
+    }
+}
+
