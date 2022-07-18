9e8e8733f0975ea6695a320c0418b12e2c8798a1
Oscar Rene Martinez <92966165+esskeeper23@users.noreply.github.com>
2022-07-18 11:25:15 -0700

    Delete PROJECT_ROCK_PAPER_SCISSORS directory

--- PROJECT_ROCK_PAPER_SCISSORS/main.js
+++
@@ -1,9 +0,0 @@
-function computerPlay(computerSelection)
-{
-  
-return computerSelection[Math.floor(Math.random()*computerSelection.length)];
-     
-}
-
-let computerSelection = ["rock", "paper", "scissors"];
-console.log(computerPlay(computerSelection));
